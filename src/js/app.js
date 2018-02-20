6c70e79f69fa18a2e7fa340d55d8706680ee0cd0
Utkarsh Malik <utkarshmalik211@gmail.com>
2018-02-20 20:54:23 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 60,
+        "value": 30,
         "density": {
           "enable": true,
           "value_area": 800
