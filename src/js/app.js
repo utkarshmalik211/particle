c537bc8d6b83eb903a18d79e3fa5f3269b4af5b3
Utkarsh Malik <utkarshmalik211@gmail.com>
2018-04-04 23:35:11 +0530

    tried to add skill graph

--- src/js/app.js
+++ src/js/app.js
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function () {
           "mode": "repulse"
         },
         "onclick": {
-          "enable": true,
+          "enable": false,
           "mode": "push"
         },
         "resize": true
